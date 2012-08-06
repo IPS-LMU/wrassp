8fefcd76156672f3076e83af01716926530db906
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2012-08-06 16:26:32 +0200

    fixed build rassp vs wrassp names + updated .Rd file for newEmu.track

--- src/wrassp.c
+++ src/wrassp.c
@@ -1,7 +1,7 @@
 #include "wrassp.h"
 #include <R_ext/Rdynload.h>
 
-void R_init_rassp(DllInfo *info)
+void R_init_wrassp(DllInfo *info)
 {
      R_CallMethodDef callMethods[] = {
 	  {"getDObj", (DL_FUNC) &getDObj, 1},
@@ -11,7 +11,7 @@ void R_init_rassp(DllInfo *info)
      R_registerRoutines(info, NULL, callMethods, NULL, NULL);
 }
 
-void R_unload_rassp(DllInfo *info)
+void R_unload_wrassp(DllInfo *info)
 {
 }
 
