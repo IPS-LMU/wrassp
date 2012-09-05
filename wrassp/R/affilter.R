75b7c7cc58f36f429ddeeab5fc564ddd40b31a6d
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2012-09-05 12:52:57 +0200

    affilter.R sets progressbar 2 null if length ==1

--- wrassp/R/affilter.R
+++ wrassp/R/affilter.R
@@ -8,8 +8,11 @@
 		stop("listOfFiles is NULL! It has to be a string or vector of file paths (min length = 1) pointing to valid file(s) to perform the given analysis function.")
 	}
 	
-	
-	pb <- txtProgressBar(min = 0, max = length(listOfFiles), style = 3)
+	if(length(listOfFiles)==1){
+		pb <- NULL
+	}else{
+		pb <- txtProgressBar(min = 0, max = length(listOfFiles), style = 3)
+	}
 
 	for (i in 1:length(listOfFiles)) {
 
@@ -20,5 +23,8 @@
 			ExplicitExt = ExplicitExt, ProgressBar=pb)
 	}
 
-	close(pb)
+	if(!(length(listOfFiles)==1)){ close(pb) }
+
+
+
 }
