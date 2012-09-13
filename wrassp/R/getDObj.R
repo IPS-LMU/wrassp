762d4abe6713fd84d44a2a05acdfd3ae5a1b8827
Lasse Bombien <lasse@phonetik.uni-muenchen.de>
2012-09-13 13:53:47 +0200

    renaming getDObj to getAsspDataObj (in R level)

--- wrassp/R/getDObj.R
+++ wrassp/R/getDObj.R
@@ -6,9 +6,9 @@
 ##' @param begin begin time (default is in ms) of segment to retrieve
 ##' @param end end time (default is in ms) of segment to retrieve
 ##' @param samples (BOOL) if set to false ms values of begin/end are sample numbers
-##' @return list object containing file data 
+##' @return list object containing file data
 ##' @author Lasse Bombien
-'getDObj' <- function(fname, begin=0, end=0, samples=FALSE) {
+'getAsspDataObj' <- function(fname, begin=0, end=0, samples=FALSE) {
     fname <- path.expand(fname)
     .External("getDObj2", fname, begin=begin, end=end, samples=samples, PACKAGE="wrassp")
 }
