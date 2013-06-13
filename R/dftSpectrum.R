08dadd9e1a69abca9b1641c5133c473dc1904eec
Lasse Bombien <lasse@phonetik.uni-muenchen.de>
2013-06-13 14:58:40 -0700

    add dftSpectrum function
    Breaking up spectrum.R stowards fixing #2.

---
+++ R/dftSpectrum.R
@@ -0,0 +1,121 @@
+##' DFT spectrum function adapted from libassp
+##'
+##' Short-term spectral analysis of the signal in <listOfFiles>
+##' using the Fast Fourier Transform. The default is to
+##' calculate an unsmoothed narrow-band spectrum with the
+##' size of the analysis window equal to the length of the
+##' FFT. The output from the FFT will be converted to a
+##' power spectrum in dB from 0 Hz up to and including the
+##' Nyquist rate.
+##' Analysis results will be written to a file with the
+##' base name of the input file and the spectrum type in
+##' lower case as extension (e.g. '.dft').
+##' Default output is in SSFF format with the
+##' spectrum type in lower case as track name.
+##' @title dftSpectrum
+##' @param listOfFiles vector of file paths to be processed by function 
+##' @param optLogFilePath path to option log file
+##' @param BeginTime = <time>: set begin of analysis interval to <time> seconds
+##' (default: begin of data)
+##' @param CenterTime = <time>: set single-frame analysis with the analysis
+##' window centred at <time> seconds; overrules BeginTime, EndTime and
+##' WindowShift options
+##' @param EndTime = <time>: set end of analysis interval to <time> seconds
+##' (default: end of data)
+##' @param Resolution = <freq>: set FFT length to the smallest value which
+##' results in a frequency resolution of <freq> Hz or better (default: 40.0)
+##' @param FftLength = <num>: set FFT length to <num> points (overrules default
+##' and 'Resolution' option)
+##' @param WindowShift = <dur>: set analysis window shift to <dur> ms
+##' (default: 5.0)
+##' @param Window = <type>: set analysis window function to <type> (default:
+##' BLACKMAN)
+##' @param Bandwidth = <freq>: set the effective analysis bandwidth to <freq>
+##' Hz (default: 0, yielding the smallest possible value given the length of
+##' the FFT)
+##' @param ToFile write results to file (default extension depends on )
+##' @param ExplicitExt set if you wish to overwride the default extension
+##' @param forceToLog option to override the package default. Option should be
+##' left alone by normal user
+##' @return nrOfProcessedFiles or if only one file to process return
+##' AsspDataObj of that file
+##' @author Raphael Winkelmann
+##' @author Lasse Bombien
+##' @useDynLib wrassp
+##' @export
+'dftSpectrum' <- function(listOfFiles = NULL, optLogFilePath = NULL,
+                       BeginTime = 0.0, CenterTime = FALSE,
+                       EndTime = 0.0, Resolution = 40.0,
+                       FftLength = 0,
+                       WindowShift = 5.0, Window = 'BLACKMAN',
+                       Bandwidth = 0.0, ## DFT specific
+                       ToFile = TRUE,
+                       ExplicitExt = NULL, forceToLog = TRUE) {
+  ## ########################
+  ## a few parameter checks and expand paths
+  
+  if (is.null(listOfFiles)) {
+    stop(paste("listOfFiles is NULL! It has to be a string or vector of file",
+               "paths (min length = 1) pointing to valid file(s) to perform",
+               "the given analysis function."))
+  }
+
+  if (is.null(optLogFilePath) && forceToLog){
+    stop("optLogFilePath is NULL! -> not logging!")
+  }else{
+    if(forceToLog){
+      optLogFilePath = path.expand(optLogFilePath)  
+    }
+  }
+  
+  if(!isAsspWindowType(Window)){
+    stop("WindowFunction of type '", Window,"' is not supported!")
+  }
+  
+
+  ## ########################
+  ## remove file:// and expand listOfFiles (SIC)
+  
+  listOfFiles = gsub("^file://","", listOfFiles)
+  listOfFiles = path.expand(listOfFiles)
+  
+  ## #######################
+  ## perform analysis
+
+  if(length(listOfFiles)==1){
+    pb <- NULL
+  }else{
+    cat('\n  INFO: applying dftSpectrum to', length(listOfFiles), 'files\n')
+    pb <- txtProgressBar(min = 0, max = length(listOfFiles), style = 3)
+  } 
+  
+  externalRes = invisible(.External("performAssp", listOfFiles, 
+    fname = "spectrum", BeginTime = BeginTime, 
+    CenterTime = CenterTime, EndTime = EndTime, 
+    Resolution = Resolution, 
+    FftLength = as.integer(FftLength),
+    WindowShift = WindowShift, Window = Window, 
+    Bandwidth = Bandwidth, 
+    ToFile = ToFile, ExplicitExt = ExplicitExt, 
+    ProgressBar = pb, PACKAGE = "wrassp"))
+
+
+  ## #########################
+  ## write options to options log file
+  if (forceToLog){
+    optionsGivenAsArgs = as.list(match.call(expand.dots = TRUE))
+    wrassp.logger(optionsGivenAsArgs[[1]], optionsGivenAsArgs[-1],
+                  optLogFilePath, listOfFiles)
+    
+  }
+  
+  ## #########################
+  ## return dataObj if length only one file
+  
+  if(!(length(listOfFiles)==1)){
+    close(pb)
+  }else{
+    return(externalRes)
+  }
+
+}
