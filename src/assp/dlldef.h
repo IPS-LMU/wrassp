31fc7f628e1f57345c5eaeaf48f654d9f5c1895b
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-10-31 13:45:04 +0100

    adapted copyright notice (closes #28)

--- src/assp/dlldef.h
+++ src/assp/dlldef.h
@@ -1,14 +1,29 @@
-//
-// C++ Interface: dlldef
-//
-// Description:
-//
-//
-// Author: Lasse Bombien,,, <lasse@cordulo>, (C) 2007
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***********************************************************************
+*                                                                      *
+* This file is part of the Advanced Speech Signal Processor library.   *
+*                                                                      *
+* Copyright (C) 2007         Lasse Bombien                             *
+*                            lasse@phonetik.uni-muenchen.de            *
+*                                                                      *
+* This library is free software: you can redistribute it and/or modify *
+* it under the terms of the GNU General Public License as published by *
+* the Free Software Foundation, either version 3 of the License, or    *
+* (at your option) any later version.                                  *
+*                                                                      *
+* This library is distributed in the hope that it will be useful,      *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+* GNU General Public License for more details.                         *
+*                                                                      *
+* You should have received a copy of the GNU General Public License    *
+* along with this library. If not, see <http://www.gnu.org/licenses/>. *
+*                                                                      *
+*----------------------------------------------------------------------*
+*                                                                      *
+* File:     dlldef.h                                                   *
+* Author:   Lasse Bombien                                              *
+*                                                                      *
+***********************************************************************/
 #ifndef _ASSP_DLLDEF_H
 #define _ASSP_DLLDEF_H
 
