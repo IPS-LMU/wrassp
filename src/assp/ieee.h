10c254265f54053799e47b081ebd0b0a7dfe7901
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-11-11 10:27:50 +0100

    updated header Copyright

--- src/assp/ieee.h
+++ src/assp/ieee.h
@@ -1,14 +1,36 @@
-/* Copyright (C) 1988-1991 Apple Computer, Inc.
- * All rights reserved.
- *
- * Machine-independent I/O routines for IEEE floating-point numbers.
- *
- */
-
-/*
- * Modified Oct. 2007 by Michel Scheffers (ms@ipds.uni-kiel.de)
- * - used data model independent integral types
- */
+/***********************************************************************
+*                                                                      *
+* This file is part of the "Advanced Speech Signal Processor" library  *
+* 'libassp'.                                                           *
+* Copyright (C) 1989 - 2011  Michael T.M. Scheffers                    *
+*                            IPdS, CAU Kiel                            *
+* Copyright (C) 2011 - 2014  Michael T.M. Scheffers                    *
+*                            ISFAS-ASW, CAU Kiel                       *
+*                            Leibnizstr. 10, R. 409                    *
+*                            24118 Kiel, Germany                       *
+*                            m.scheffers@isfas.uni-kiel.de             *
+*                                                                      *
+* This library is free software: you can redistribute it and/or modify *
+* it under the terms of the GNU General Public License as published by *
+* the Free Software Foundation, either version 3 of the License, or    *
+* (at your option) any later version.                                  *
+*                                                                      *
+* This library is distributed in the hope that it will be useful,      *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                 *
+* See the GNU General Public License for more details.                 *
+*                                                                      *
+* You should have received a copy of the GNU General Public License    *
+* along with this library. If not, see <http://www.gnu.org/licenses/>. *
+*                                                                      *
+*----------------------------------------------------------------------*
+*                                                                      *
+* File    : ieee.h                                                     *
+* Contents:                                                            *
+* Authors : Michel Scheffers                                           *
+*                                                                      *
+***********************************************************************/
+/* $Id:  Exp $ */
 
 #ifndef _IEEE_H
 #define _IEEE_H
